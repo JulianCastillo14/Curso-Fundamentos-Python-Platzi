42cbdd0d0a220dbe44ad588069e6838bd2e9153a
JulianCastillo14 <juliancast140502@gmail.com>
2023-09-18 12:51:15 -0500

    Agregando algunos comentarios a 009_arith.py

--- 009_arith.py
+++ 009_arith.py
@@ -1,12 +1,12 @@
-print(10 + 10)
-print(10 - 5)
-print(10 * 2)
-print(10 / 2)
-print(10 % 2)
-print(10 / 3)
+print(10 + 10) #Suma
+print(10 - 5)  #Resta
+print(10 * 2)  #Multilicaciín
+print(10 / 2)  #División
+print(10 % 2)  #Residuo
+print(10 / 3) 
 print(10 % 3)
-print(10 // 3)
-print(2 ** 3)
+print(10 // 3) #Dividir y solo dar la parte entera
+print(2 ** 3)  #Potenciación
 print(2 ** 3 + 3 -7 / 1 // 4)
 print(8 + 3 - 1)
 
